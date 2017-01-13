3962e39c8b0397bef94b90f521875645fbe4ed5b
Archie Yu <yuyq96@126.com>
2017-01-13 12:24:38 +0800

    add AddCourseFromWebView

---
+++ Course/AddCourseFromWebViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddCourseFromWebViewController.swift
+//  Course
+//
+//  Created by Archie Yu on 2017/1/13.
+//  Copyright © 2017年 Archie Yu. All rights reserved.
+//
+
+import UIKit
+
+class AddCourseFromWebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
