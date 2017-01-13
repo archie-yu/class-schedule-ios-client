8e8fa23c8caba41a41618d90fd7df8f6c93045c1
CedricXing <cedricxing0513@gmail.com>
2017-01-13 15:37:40 +0800

    delete course and can add course details now

--- Course/CourseDetailViewController.swift
+++ Course/CourseDetailViewController.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
+import CourseModel
 
-class CourseDetailViewController: UIViewController {
-
+class CourseDetailViewController: UIViewController ,UITextViewDelegate{
+    var weekday = -1
+    var courseNo = -1
+    var courseName = ""
+    let weekdays = ["周一","周二","周三","周四","周五"]
+    
+    @IBOutlet weak var teachernameField: UITextField!
+    
+    @IBOutlet weak var weekdayField: UITextField!
+    @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var timeField: UITextField!
+    
+    @IBOutlet weak var coursenameLabel: UILabel!
+    
+    @IBOutlet weak var courseDetails: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        courseDetails.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -31,5 +48,72 @@ class CourseDetailViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let course = everydayCourseList[weekday][courseNo]
+        courseName = course.course
+        
+        coursenameLabel.text = courseName
+        teachernameField.text = course.teacher
+        weekdayField.text = weekdays[course.weekday - 1]
+        locationField.text = course.location
+        timeField.text = String(course.begin) + "-" + String(course.end) + "节"
+        if course.courseDetails != ""{
+            courseDetails.text = course.courseDetails
+        }
+    }
 
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView == courseDetails{
+            if textView.text == "课程详细信息"{
+                textView.text = ""
+            }
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView == courseDetails{
+            if textView.text == ""{
+                textView.text = "课程详细信息"
+            }
+            else{
+                for course in courseList{
+                    if course.course == courseName{
+                        course.courseDetails = courseDetails.text
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func DeleteCourse(_ sender: UIButton) {
+        //删除课程前 弹出警告框
+        let alertController = UIAlertController(title: "删除课程", message: "删除后将不可恢复", preferredStyle:.actionSheet)
+        
+        // 设置2个UIAlertAction
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        let deleteAction = UIAlertAction(title: "删除", style: .destructive, handler: ConfirmDeleteCourse)
+        
+        // 添加到UIAlertController
+        alertController.addAction(cancelAction)
+        alertController.addAction(deleteAction)
+        
+        // 弹出
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func ConfirmDeleteCourse(_ alert: UIAlertAction) -> Void{
+        let course = everydayCourseList[weekday][courseNo]
+        let index = courseList.index(of: course)
+        courseList.remove(at: index!)
+        everydayCourseList[weekday].remove(at: courseNo)
+        
+        
+        let filePath = courseDataFilePath()
+        NSKeyedArchiver.archiveRootObject(courseList, toFile: filePath)
+        
+        self.dismiss(animated: true, completion: nil)
+        
+        
+    }
 }
